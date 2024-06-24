0f8588922c2557ed6c8aa9d5f19e6990757e9c50
Slavomir Sidor <slavomir.sidor@gmail.com>
2024-06-24 21:06:58 +0200

    Alarm default data source

--- src/WDX/Schema/Model/Instance/AlarmInstance.ts
+++ src/WDX/Schema/Model/Instance/AlarmInstance.ts
@@ -10,10 +10,13 @@
 
 import {Type} from './Type';
 import {Instance} from './Instance';
-import { AlarmExecutionOptions } from './AlarmExecutionOptions';
+import {AlarmExecutionOptions} from './AlarmExecutionOptions';
+import {DataSourceOptions} from './DataSourceOptions';
 
 export class AlarmInstance extends Instance {
   public code: string = Type.Alarm;
   public namespace: string = Type.Controller;
   public executionOptions?: AlarmExecutionOptions = new AlarmExecutionOptions();
+  public dataSourceOptions?: DataSourceOptions =
+      new DataSourceOptions('default');
 }
